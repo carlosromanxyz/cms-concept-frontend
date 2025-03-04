5d2dd3c27b7942c00012ae4263538719b28fb21a
carlosromanxyz <hola@carlosroman.xyz>
2025-03-04 12:47:42 -0300

    Add Strapi SDK initialization in strapi.ts

---
+++ src/lib/strapi.ts
@@ -0,0 +1,9 @@
+import Strapi from 'strapi-sdk-js';
+
+export const strapi = new Strapi({
+  // Options goes here
+  // Ex: url: 'http://localhost:1337'
+  // Ex for prefix: /api
+  // All as default
+  // Check more on https://strapi-sdk-js.netlify.app/api/options
+});
