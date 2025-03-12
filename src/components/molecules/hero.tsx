e310e00c35669b67bb767f55247125293467a33a
carlosromanxyz <hola@carlosroman.xyz>
2025-03-12 10:34:34 -0300

    Refactor Hero component to remove AspectRatio wrapper around Image

--- src/components/molecules/hero.tsx
+++ src/components/molecules/hero.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { AspectRatio } from '@radix-ui/react-aspect-ratio'
 import Image from 'next/image'
 import { Badge } from '../ui/badge';
 
@@ -15,15 +14,13 @@ interface IHero {
 export default function Hero({ imageSrc, imageAlt, imageWidth, imageHeight, heading, title }: IHero) {
   return (
     <section className="relative">
-      <AspectRatio ratio={16 / 9}>
-        <Image
-          src={imageSrc}
-          width={imageWidth}
-          height={imageHeight}
-          alt={imageAlt || 'Image alt text'}
-          className='object-cover w-full h-full'
-        />
-      </AspectRatio>
+      <Image
+        src={imageSrc}
+        width={imageWidth}
+        height={imageHeight}
+        alt={imageAlt || 'Image alt text'}
+        className='object-cover w-full h-full'
+      />
       <div className=''>
         <Badge>{heading}</Badge>
         <h1 className='text-4xl font-bold'>{title}</h1>
