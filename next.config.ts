b0ea6686075bec5376e02484bfcf2494fa27851f
carlosromanxyz <hola@carlosroman.xyz>
2025-03-12 18:10:03 -0300

    Add Docker support and environment configuration files

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Enable serverless deployment
   images: {
     remotePatterns: [
       {
         protocol: 'http',
-        hostname: 'cms-concept-backend',
+        // hostname: 'cms-concept-backend',
+        hostname: 'localhost',
         port: '1337',
         pathname: '/uploads/**',
       }
