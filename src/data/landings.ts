9ced350300da9a6534ff96ffe64da18ca64d7130
carlosromanxyz <hola@carlosroman.xyz>
2025-03-12 10:56:51 -0300

    Update landing page data fetching to use client instead of strapi

--- src/data/landings.ts
+++ src/data/landings.ts
@@ -1,8 +1,8 @@
-import { strapi } from "@/lib/strapi";
+import { client } from "@/lib/strapi";
 
 export const getAllLandings = async () => {
   try {
-    const response = await strapi.find('landings');
+    const response = await client.collection('landings');
     return response;
   } catch (error) {
     console.error("Error fetching landing page:", error);
